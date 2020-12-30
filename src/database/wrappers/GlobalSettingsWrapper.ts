793a1a8170adf25c0746ae877ca5bc6040efee3d
Jet132 <37117573+Jet132@users.noreply.github.com>
2020-12-30 11:52:44 +0100

    refactor(db): GlobalSettingsWrapper

--- src/database/wrappers/GlobalSettingsWrapper.ts
+++ src/database/wrappers/GlobalSettingsWrapper.ts
@@ -1,18 +1,22 @@
-import {filter, map} from 'rxjs/operators';
-import {ExitCode, exitWithError} from '../../utils/process';
+import {filter, tap} from 'rxjs/operators';
+import {container} from 'tsyringe';
+import {FindConditions} from 'typeorm';
+import ScopedLogger from '../../logger/ScopedLogger';
 import GlobalSettingsEntity from '../entities/GlobalSettingsEntity';
 import type GlobalSettingsManager from '../managers/GlobalSettingsManager';
 import {SyncStream} from '../synchronizer/CacheSynchronizer';
 import CacheEntityWrapper from '../wrapper/CacheEntityWrapper';
 
-export default class GlobalSettingsWrapper
-  extends CacheEntityWrapper<GlobalSettingsEntity, GlobalSettingsManager, GlobalSettingsEntity>
-  implements GlobalSettingsEntity {
+export default class GlobalSettingsWrapper extends CacheEntityWrapper<
+  GlobalSettingsEntity,
+  GlobalSettingsManager
+> {
   get version(): number {
     return this.entity.version;
   }
 
   get botToken(): string {
+    this.logger.warn('Bot token was accessed.');
     return this.entity.botToken;
   }
 
@@ -20,30 +24,39 @@ export default class GlobalSettingsWrapper
     return this.entity.prefix;
   }
 
-  protected uniqueConditions = {};
+  set prefix(prefix: string) {
+    this.updateEntity({prefix});
+  }
+
+  protected get uniqueConditions(): FindConditions<GlobalSettingsEntity> {
+    return {
+      version: this.version,
+    };
+  }
+
+  private readonly logger: ScopedLogger;
 
   constructor(
     manager: GlobalSettingsManager,
     syncStream: SyncStream<GlobalSettingsEntity>,
-    entity: GlobalSettingsEntity
+    entity: GlobalSettingsEntity,
+    logger = container.resolve(ScopedLogger)
   ) {
     super(
       manager,
       syncStream.pipe(
-        map(newEntity => {
-          if (!newEntity) this.manager.refetch();
-          return newEntity;
+        tap(newEntity => {
+          if (newEntity) return;
+          this.manager.refetch();
         }),
         filter((newEntity): newEntity is GlobalSettingsEntity => !!newEntity)
       ),
       entity
     );
+    this.logger = logger;
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  protected createDefaultEntity(): never {
-    throw new Error('Cannot create default GlobalSettings entity.');
-  }
+  protected createDefaultEntity = undefined;
 
   // eslint-disable-next-line class-methods-use-this
   isEntityUseless(): boolean {
