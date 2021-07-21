cc38ca8f59033367c1b2f5e54540d0e36f440c03
Jet132 <37117573+Jet132@users.noreply.github.com>
2021-07-21 18:09:07 +0200

    fix(megalog): attachment-send size restriction calculation
    Take the cumulative size instead of the individual size of each attachment.

--- src/modules/megalog/eventTypes/message/attachmentSendEventType.ts
+++ src/modules/megalog/eventTypes/message/attachmentSendEventType.ts
@@ -3,6 +3,7 @@ import {MessageEmbed} from 'discord.js';
 import {MessageMegalogEventCategoryName} from '.';
 import {MegalogEventType} from '../../eventType/MegalogEventType';
 
+// Maximum size the rest of the send request package will use in bytes
 const MESSAGE_REQUEST_OVERHEAD_SIZE = 32 + 2 ** 10;
 
 const MEBI_BYTE = 2 ** 20;
@@ -14,6 +15,13 @@ const MAX_UPLOAD_SIZE = [
   100 * MEBI_BYTE - MESSAGE_REQUEST_OVERHEAD_SIZE,
 ] as const;
 
+function toMegaByteString(size: number) {
+  return `${
+    Math.round((size / MEBI_BYTE + Number.EPSILON) * 100) / 100
+    // Discord calls actual mebibytes "megabytes" so we do the same here
+  }MB`;
+}
+
 const attachmentSendEventTypeName = 'attachment-send';
 
 export function attachmentSendEventType(
@@ -22,7 +30,7 @@ export function attachmentSendEventType(
   return {
     name: attachmentSendEventTypeName,
     description:
-      'When someone sends a message with attachments those attachments get logged. This can be combined with the `message-delete-` events to see attachments of deleted messages.',
+      'When someone sends a message with attachments those attachments get logged. This can be combined with the `message-delete-*` events to see attachments of deleted messages.',
     category: MessageMegalogEventCategoryName,
     clientEventName: 'message',
     processClientEvent: async message => {
@@ -31,37 +39,44 @@ export function attachmentSendEventType(
         if (message.author.id === message.client.user?.id && message.channel.id === channel.id)
           return;
         const sizeLimit = MAX_UPLOAD_SIZE[channel.guild.premiumTier];
-        const sendableAttachments = message.attachments.filter(
-          attachment => attachment.size <= sizeLimit
-        );
-        const unsendableAttachments = message.attachments.filter(
-          attachment => attachment.size > sizeLimit
-        );
+
+        const attachments = message.attachments.array();
+        let cumulativeSize = 0;
+        const firstUnsendableIndex = attachments.findIndex(attachment => {
+          cumulativeSize += attachment.size;
+          return cumulativeSize > sizeLimit;
+        });
+        if (firstUnsendableIndex !== -1) cumulativeSize -= attachments[firstUnsendableIndex].size;
+
+        const sendableAttachments =
+          firstUnsendableIndex < 0 ? attachments : attachments.slice(0, firstUnsendableIndex);
+        const unsendableAttachments =
+          firstUnsendableIndex < 0 ? [] : attachments.slice(firstUnsendableIndex);
 
         const embed = new MessageEmbed()
           .setDescription(
-            `${sendableAttachments.size}/${message.attachments.size} attachment${
-              sendableAttachments.size === 1 ? '' : 's'
-            } from [a message](${message.url}) sent by ${message.author} in ${message.channel}.`
+            `${sendableAttachments.length}/${attachments.length} attachment${
+              sendableAttachments.length === 1 ? '' : 's'
+            } (${toMegaByteString(cumulativeSize)}) from [a message](${message.url}) sent by ${
+              message.author
+            } in ${message.channel}.`
           )
           .setColor(globalSettings.getColor(ColorType.Default))
           .setTimestamp(message.createdAt)
           .setFooter(`Message ID: ${message.id} | ${attachmentSendEventTypeName}`);
-        if (unsendableAttachments.size > 0)
+        if (unsendableAttachments.length > 0)
           embed.addField(
-            `Omitted oversized attachments [${unsendableAttachments.size}]`,
+            `Omitted attachments [${unsendableAttachments.length}]`,
             unsendableAttachments
               .map(
                 attachment =>
-                  `[${attachment.name}](${attachment.url}) - ${(attachment.size / MEBI_BYTE)
-                    // Discord calls actual mebibytes "megabytes" so we do the same here
-                    .toPrecision(2)}MB`
+                  `[${attachment.name}](${attachment.url}) - ${toMegaByteString(attachment.size)}`
               )
               .join('\n')
           );
         await channel.send({
           embed,
-          files: sendableAttachments.array(),
+          files: sendableAttachments,
         });
       };
     },
